6e54c7197764e4448c2d4302312e851028302d81
hangsunlee <hslee22@sen.go.kr>
2018-04-10 15:08:29 +0900

    자신이 업로드한 파일만 다운로드 표시함.

--- login.php
+++ login.php
@@ -8,12 +8,12 @@
 <div id="login">
     <form action="./login_ok.php" method="post">
       <dl>
-          <dt><label for="id">아이디</label></dt><dd></dd><input type="text" id="id" name="id"></dd>
-          <dt><label for="password">비밀번호</label></dt><dd><input type="password" id="password"name="password"></dd>
+          <dt><label for="id">아이디</label></dt><dd><input type="text" id="id" name="id"></dd>
+          <dt class="pw"><label for="password">비밀번호</label></dt><dd><input type="password" id="password"name="password"></dd>
       </dl>
-    <button type="submit">로그인</button>
+    <button id="login_btn" type="submit">로그인</button>
 
 </form>
-    <button id="close">닫기</button>
+    <a href="#" id="close"><span class="blind">닫기</span></a>
 </div>
 
